784ec867a7ac9a877ef6f5d63dcd13ab1c29a135
carrotpig <wyzhouzhiqi@163.com>
2019-10-23 17:26:57 +0800

    add create progect

---
+++ src/views/Mine/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="main">
+        <Header />
+            <div id="content">
+                <Login />
+            </div>
+        <Tabber />
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+import Header from '@/components/Header'
+import Tabber from '@/components/Tabber'
+import Login from '@/components/Login'
+export default {
+    data() {
+        return {
+
+        }
+    },
+    components: {
+        Header,
+        Tabber,
+        Login
+    }
+}
+</script>
+
+<style scoped lang="stylus" scoped>
+</style>
+
